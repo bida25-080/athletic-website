2a57e718d3ebfbf75fdfd4f7eea5629d33989e21
bida25-080 <bida25-080@thuto.bac.ac.bw>
2026-05-25 19:42:40 +0200

    Update athletic website

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("ATHLETIX Website Loaded");
+
+function showMessage() {
+  alert("Thank you for contacting ATHLETIX! We will get back to you soon.");
+}
+
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+}
